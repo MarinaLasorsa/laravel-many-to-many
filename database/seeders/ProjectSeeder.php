69ec5edfb4da9b9e1c51e56410c2b3621dda7229
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-06-07 17:43:56 +0200

    ADD: seeder rules for project and technology to create random relations in pivot table

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Project;
 use App\Models\Type;
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -17,10 +18,12 @@ class ProjectSeeder extends Seeder
     public function run(Faker $faker): void
     {
 
-        $types = Type::all();
-        $ids = $types->pluck('id')->all();
+        $types = Type::all(); //collection di types
+        $types_ids = $types->pluck('id')->all(); //array di id dei types. (pluck() fa una collection ma all() la rende un array)
 
-        for($i = 0; $i < 10; $i++) {
+        $technologies_ids = Technology::all()->pluck('id')->all();
+
+        for($i = 0; $i < 30; $i++) {
             
             $project = new Project();
 
@@ -30,10 +33,15 @@ class ProjectSeeder extends Seeder
             $project->slug = Str::slug($title);
             $project->description = $faker->text(500);
             $project->url = $faker->url();
-            $project->type_id = $faker->optional()->randomElement($ids);
+            $project->type_id = $faker->optional()->randomElement($types_ids);
 
             $project->save();
 
+            //da fare dopo il salvataggio per abbinare gli ids dei projects a quelli delle technologies:
+            //prende ids a caso in numero random dall'array
+            $random_technologies_ids = $faker->randomElements($technologies_ids);
+            //li abbina all'id del project
+            $project->technologies()->attach($random_technologies_ids);
         }
     }
 }
--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,6 +14,17 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = ['html', 'css', 'javascript', 'php', 'python', 'vue', 'laravel'];
+
+        foreach($technologies as $technology_name) {
+
+            $new_technology = new Technology();
+
+            $new_technology->name = $technology_name;
+            $new_technology->slug = Str::slug($technology_name);
+
+            $new_technology->save();
+        }
+
     }
 }
